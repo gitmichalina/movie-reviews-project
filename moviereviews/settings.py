d1c58c75a85c2ff1a20173f2dfa1743222b644d9
Michalina Maria <michalina.dev@gmail.com>
2024-02-04 22:34:13 +0100

    news app with model and view

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "movie.apps.MovieConfig",
+    "news.apps.NewsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
