4255dd599ddcf21a75242bee0e4f03bfba1d469f
Michalina Maria <michalina.dev@gmail.com>
2024-02-05 00:16:06 +0100

    login firm - pass eq checking

--- accounts/views.py
+++ accounts/views.py
@@ -17,3 +17,7 @@ def signupaccount(request):
             user.save()
             login(request, user)
             return redirect('home')
+        else:
+            return render(request, 'signupaccount.html',
+                          {'form': UserCreationForm,
+                           'error': 'Passwords do not match'})
