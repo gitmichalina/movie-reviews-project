ce855d8d0a50d35a5d260726faf6023e56ef371b
Michalina Maria <michalina.dev@gmail.com>
2024-02-04 23:45:39 +0100

    move detail page with links from home

--- movie/views.py
+++ movie/views.py
@@ -1,13 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Movie
+from django.shortcuts import get_object_or_404
 
-
-# def home(request):
-#     return HttpResponse('<h1>Welcome to Home Page</h1>')
-
-# def home(request):
-#     return render(request, 'home.html', {'name': 'Greg'})
 
 def home(request):
     searchTerm = request.GET.get('searchMovie')
@@ -17,6 +12,12 @@ def home(request):
         movies = Movie.objects.all()
     return render(request, 'home.html',
                   {'searchTerm': searchTerm, 'movies': movies})
+
+
+def detail(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    return render(request, 'detail.html',
+                  {'movie': movie})
 
 
 def signup(request):
