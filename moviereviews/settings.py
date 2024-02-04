83f083a69cb8afb3f43fc7b41efa9de4ff7d470e
Michalina Maria <michalina.dev@gmail.com>
2024-02-05 00:08:52 +0100

    accounts app with login form

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     "movie.apps.MovieConfig",
     "news.apps.NewsConfig",
+    "accounts.apps.AccountsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
